b8f4a7b3bdc614869e66e58f6a5c4ffb6857bf8f
Giacomo Caldironi <giacomo.caldironi@dgroove.it>
2021-05-20 17:58:20 +0200

    aggiunta card

--- Postock/src/app/stocks/stocks.component.ts
+++ Postock/src/app/stocks/stocks.component.ts
@@ -9,17 +9,28 @@ import { StockService } from 'src/services/stock.service';
 export class StocksComponent implements OnInit {
   public performanceDate: string = "";
   public yesterdayPrice: string = "";
+  public lastQuotationVariation: string = "";
+  public quotationLastPrice: string = "";
+  public valueIsUp = true;
   constructor(private stockService: StockService) { }
 
   ngOnInit(): void {
+    this.getQBUO();
+  }
+
+  getQBUO() {
     this.stockService.getQBUO().subscribe(data => {
       var parser = new DOMParser();
       var doc = parser.parseFromString(data.contents, 'text/html');
-
+      // console.log(doc.body);
       this.performanceDate = doc.body.getElementsByClassName("performance-date")[0].textContent?.toString() || "";
-      this.yesterdayPrice = doc.body.getElementsByClassName("yesterday-price")[0].getElementsByClassName("value")[0].textContent || "";
-    })
+      this.lastQuotationVariation = doc.body.getElementsByClassName("quotation last-quot-var")[0].getElementsByClassName("value")[0].textContent || "";
+      this.quotationLastPrice = doc.body.getElementsByClassName("quotation last-price")[0].getElementsByClassName("value")[0].textContent || "";
+      this.yesterdayPrice = doc.body.getElementsByClassName("yesterday-price")[0].textContent?.toString() || "";
 
-
+      if (this.lastQuotationVariation.indexOf("-") >= 0) {
+        this.valueIsUp = false;
+      }
+    });
   }
 }
